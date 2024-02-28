292b5ee030c6731bca62540ca3e2a20a717810c2
yektaokdan <57546227+yektaokdan@users.noreply.github.com>
2024-02-28 16:59:05 +0300

    Initial commit

---
+++ DonutChartSUI/DonutChartSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DonutChartSUIApp.swift
+//  DonutChartSUI
+//
+//  Created by yekta on 28.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct DonutChartSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
